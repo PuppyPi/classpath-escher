315cb5fe4c5c248086e8bd9b2ca93139436eb482
PuppyPi <puppyofcodez@gmail.com>
2023-11-15 01:50:51 +0000

    Organize imports X33

--- src/gnu/x11/Display.java
+++ src/gnu/x11/Display.java
@@ -7,7 +7,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.InetAddress;
-import java.net.Socket;
 import java.net.UnknownHostException;
 import java.util.ArrayList;
 import java.util.HashMap;
