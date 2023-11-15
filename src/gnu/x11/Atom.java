69c8fb07e405d798b364b55f267fa223de4a899d
PuppyPi <puppyofcodez@gmail.com>
2023-11-15 00:20:40 +0000

    This change is to let Atoms and Resources be in HashSet's or keys in HashMap's! :D

--- src/gnu/x11/Atom.java
+++ src/gnu/x11/Atom.java
@@ -376,4 +376,24 @@ public class Atom {
       this.name = name;
   }  
   
+
+  @Override public int hashCode () {
+    final int prime = 31;
+    int result = 1;
+    result = prime * result + id;
+    return result;
+  }
+
+  @Override public boolean equals (Object obj) {
+    if (this == obj)
+      return true;
+    if (obj == null)
+      return false;
+    if (getClass () != obj.getClass ())
+      return false;
+    Atom other = (Atom) obj;
+    if (id != other.id)
+      return false;
+    return true;
+  }
 }
--- src/gnu/x11/Resource.java
+++ src/gnu/x11/Resource.java
@@ -53,4 +53,24 @@ public abstract class Resource {
   public Display getDisplay() {
     return display;
   }
+
+  @Override public int hashCode () {
+    final int prime = 31;
+    int result = 1;
+    result = prime * result + id;
+    return result;
+  }
+
+  @Override public boolean equals (Object obj) {
+    if (this == obj)
+      return true;
+    if (obj == null)
+      return false;
+    if (getClass () != obj.getClass ())
+      return false;
+    Resource other = (Resource) obj;
+    if (id != other.id)
+      return false;
+    return true;
+  }
 }
