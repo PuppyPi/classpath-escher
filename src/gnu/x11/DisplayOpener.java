1a4951ede0bc9ca3771c5e7c6408f611aa7f7429
PuppyPi <puppyofcodez@gmail.com>
2023-12-01 03:53:00 +0000

    This isn't really very useful anymore; UnixSocketOpener is the real key thing to refactor :3

--- src/gnu/x11/DisplayOpener.java
+++
@@ -1,7 +0,0 @@
-package gnu.x11;
-
-import java.io.IOException;
-
-public interface DisplayOpener {
-  public Display open (String hostname, int displayNumber, int screenNumber) throws IOException;
-}
