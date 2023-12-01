a81476571e284a6986630dbc6ae4227047ac9521
PuppyPi <puppyofcodez@gmail.com>
2023-12-01 03:48:45 +0000

    Make our existing one use the new API so it can benefit from the improved Display Name parsing and handling, namely the locating of /tmp/.X11-unix/X#!  \:D/

--- src-jnr-unixsockets/gnu/x11/UnixDomainSocketSupportingDisplayOpenerByJNRUnixSockets.java
+++ src-jnr-unixsockets/gnu/x11/UnixDomainSocketSupportingDisplayOpenerByJNRUnixSockets.java
@@ -1,38 +1,16 @@
 package gnu.x11;
 
+import java.io.File;
 import java.net.Socket;
+
 import jnr.unixsocket.UnixSocket;
 import jnr.unixsocket.UnixSocketAddress;
 import jnr.unixsocket.UnixSocketChannel;
 
-public enum UnixDomainSocketSupportingDisplayOpenerByJNRUnixSockets implements DisplayOpener {
+public enum UnixDomainSocketSupportingDisplayOpenerByJNRUnixSockets implements UnixSocketOpener {
   I;
 
-  /**
-   * @param screenNumber
-   *          -1 if not specified, {@link #defaultScreenNumber} will be 0
-   * @throws EscherServerConnectionException
-   * @see <a href="XOpenDisplay.html">XOpenDisplay</a>
-   */
-  @Override public Display open (String hostname, int displayNumber,
-                                 int screenNumber) {
-    System.err.println ("Deisplay::<init>: " + hostname + ":" + displayNumber
-                        + "." + screenNumber);
-    
-    Socket socket;
-    
-    if (hostname.startsWith ("/")) {
-      String displayNmae = hostname + ":" + displayNumber
-                           + (screenNumber != -1 ? "." + screenNumber : "");
-      UnixSocketAddress address = new UnixSocketAddress (displayNmae);
-      UnixSocketChannel channel = UnixSocketChannel.open (address);
-      socket = new UnixSocket (channel);
-    }
-    else {
-      socket = new Socket (hostname,
-                                                        6000 + displayNumber);
-    }
-    
-    return new Display (X11Socketlike.fromJRESocket (socket), hostname, displayNumber, screenNumber);
+  @Override public X11Socketlike open (File path) {
+    return X11Socketlike.fromJRESocket(new UnixSocket(UnixSocketChannel.open(new UnixSocketAddress(path))));
   }
 }
