0013c06c6399bee9b37d4e7b2596ad3e7b09a84d
PuppyPi <puppyofcodez@gmail.com>
2023-12-01 03:35:03 +0000

    Aaaaaaaaaand convert the test for it!! :D
    Weeeeeee
    • Removed the connect() tests; those can only work with *massive bytecode rewriting/engineering* whiiiiich is not exactly portable 8))''
    • Removed error message testing since that's not really that important X3 and isn't localizable well
    • Converted the exception model for our alternate one that involves no special exceptions
    • Backported it from JUnit 5 to JUnit 4!
    
    \:D/

--- test/publicapi/display/DisplayNameTest.java
+++ test/publicapi/display/DisplayNameTest.java
@@ -1,141 +1,90 @@
 package publicapi.display;
 
-import gnu.x11.DisplayName;
-import gnu.x11.*;
-import java.io.*;
-import java.net.*;
-import mockit.*;
-import org.junit.jupiter.api.*;
-import org.newsclub.net.unix.*;
-
 import static gnu.x11.DisplayName.*;
-import static org.assertj.core.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.*;
+import gnu.x11.DisplayName;
+
+import java.io.File;
+
+import org.junit.Test;
 
 public class DisplayNameTest {
+  protected static void assertThrows(Class<? extends Throwable> c, Runnable r) {
+    try {
+      r.run();
+      throw new AssertionError();
+    }
+    catch (Throwable t) {
+      assert c.isInstance(t);
+    }
+  }
+  
   @Test
-  void parse_null_fails() {
-    NullPointerException exception = assertThrows(NullPointerException.class, () -> parse(null));
-    assertThat(exception).hasMessage("convention is marked non-null but is null");
+  public void parse_null_fails() {
+    assertThrows(NullPointerException.class, () -> parse(null));
   }
 
   @Test
-  void parse_empty_fails() {
-    IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> parse(""));
-    assertThat(exception).hasMessage("convention must not be blank.");
+  public void parse_empty_fails() {
+    assertThrows(IllegalArgumentException.class, () -> parse(""));
   }
 
   @Test
-  void parse_negative_displayNumber_fails() {
-    X11ClientException exception = assertThrows(X11ClientException.class, () -> parse(":-1"));
-    assertThat(exception).hasMessage("expected displayNumber > 0 but was \"-1\".");
+  public void parse_negative_displayNumber_fails() {
+    assertThrows(IllegalArgumentException.class, () -> parse(":-1"));
   }
 
   @Test
-  void parse_negative_screenNumber_fails() {
-    X11ClientException exception = assertThrows(X11ClientException.class, () -> parse(":1.-1"));
-    assertThat(exception).hasMessage("expected screenNumber > 0 but was \"-1\".");
+  public void parse_negative_screenNumber_fails() {
+    assertThrows(IllegalArgumentException.class, () -> parse(":1.-1"));
   }
 
   @Test
-  void parse_hostName() {
+  public void parse_hostName() {
     gnu.x11.DisplayName name = parse("hostName");
-    assertThat(name.getHostName()).isEqualTo("hostName");
-    assertThat(name.getDisplayNumber()).isEqualTo(0);
-    assertThat(name.getScreenNumber()).isEqualTo(0);
-    assertThat(name.toString()).isEqualTo("hostName:0.0");
-    assertThat(name.getSocketFile()).isNull();
+    assert name.getHostName().equals("hostName");
+    assert name.getDisplayNumber() == 0;
+    assert name.getScreenNumber() == 0;
+    assert name.toString().equals("hostName:0.0");
+    assert name.getSocketFile() == null;
   }
 
   @Test
-  void parse_hostName_displayNumber() {
+  public void parse_hostName_displayNumber() {
     gnu.x11.DisplayName name = parse("hostName:2");
-    assertThat(name.getHostName()).isEqualTo("hostName");
-    assertThat(name.getDisplayNumber()).isEqualTo(2);
-    assertThat(name.getScreenNumber()).isEqualTo(0);
-    assertThat(name.toString()).isEqualTo("hostName:2.0");
-    assertThat(name.getSocketFile()).isNull();
+    assert name.getHostName().equals("hostName");
+    assert name.getDisplayNumber() == 2;
+    assert name.getScreenNumber() == 0;
+    assert name.toString().equals("hostName:2.0");
+    assert name.getSocketFile() == null;
   }
 
   @Test
-  void parse_hostName_displayNumber_screenNumber() {
+  public void parse_hostName_displayNumber_screenNumber() {
     gnu.x11.DisplayName name = parse("hostName:2.1");
-    assertThat(name.getHostName()).isEqualTo("hostName");
-    assertThat(name.getDisplayNumber()).isEqualTo(2);
-    assertThat(name.getScreenNumber()).isEqualTo(1);
-    assertThat(name.toString()).isEqualTo("hostName:2.1");
-    assertThat(name.getSocketFile()).isNull();
+    assert name.getHostName().equals("hostName");
+    assert name.getDisplayNumber() == 2;
+    assert name.getScreenNumber() == 1;
+    assert name.toString().equals("hostName:2.1");
+    assert name.getSocketFile() == null;
   }
 
   @Test
-  void parse_displayNumber() {
+  public void parse_displayNumber() {
     gnu.x11.DisplayName name = parse(":2");
-    assertThat(name.getHostName()).isNull();
-    assertThat(name.getDisplayNumber()).isEqualTo(2);
-    assertThat(name.getScreenNumber()).isEqualTo(0);
-    assertThat(name.toString()).isEqualTo(":2.0");
-    assertThat(name.getSocketFile()).isEqualTo(new File("/tmp/.X11-unix/X2"));
+    assert name.getHostName() == null;
+    assert name.getDisplayNumber() == 2;
+    assert name.getScreenNumber()== 0;
+    assert name.toString().equals(":2.0");
+    assert name.getSocketFile().equals(new File("/tmp/.X11-unix/X2"));
   }
 
   @Test
-  void parse_displayNumber_screenNumber() {
+  public void parse_displayNumber_screenNumber() {
     DisplayName name = parse(":2.1");
-    assertThat(name.getHostName()).isNull();
-    assertThat(name.getDisplayNumber()).isEqualTo(2);
-    assertThat(name.getScreenNumber()).isEqualTo(1);
-    assertThat(name.toString()).isEqualTo(":2.1");
-    assertThat(name.getSocketFile()).isEqualTo(new File("/tmp/.X11-unix/X2"));
-  }
-
-  @Test
-  void connect_unix_exception_creating() throws IOException {
-    IOException cause = new IOException("expected");
-    new MockUp<AFUNIXSocketAddress>() {
-      @Mock
-      public void $init(File socketFile) throws IOException {
-        throw cause;
-      }
-    };
-    X11ClientException result = assertThrows(X11ClientException.class, () -> parse(":0").connect());
-    assertThat(result).hasMessage("Failed to create connection to \":0.0\".");
-    assertThat(result).hasCause(cause);
-  }
-
-  @Test
-  void connect_unix_socket(@Mocked Display display, @Mocked AFUNIXSocket socket, @Mocked AFUNIXSocketAddress address) throws IOException {
-    parse(":2.1").connect();
-    new Verifications() {{
-      AFUNIXSocket.connectTo((AFUNIXSocketAddress) any);
-      new Display((Socket) any, 2, 1);
-    }};
-  }
-
-  @Test
-  void connect_tcp_socket_unknown_host(@Mocked InetAddress address) throws UnknownHostException {
-    UnknownHostException cause = new UnknownHostException("expected");
-    new Expectations() {{
-      InetAddress.getByName("hostName"); result = cause;
-    }};
-    X11ClientException exception = assertThrows(X11ClientException.class, () -> parse("hostName:2.1").connect());
-    assertThat(exception).hasMessage("Failed to create connection to \"hostName:2.1\".");
-    assertThat(exception).hasCause(cause);
-  }
-
-  @Test
-  void connect_tcp_socket_hostname(@Mocked Display display, @Mocked Socket socket, @Mocked InetAddress address) throws IOException {
-    parse("hostName:2.1").connect();
-    new Verifications() {{
-      InetAddress.getByName("hostName");
-      new Display((Socket) any, 2, 1);
-    }};
-  }
-
-  @Test
-  void connect_tcp_socket_localhost(@Mocked Display display, @Mocked Socket socket, @Mocked InetAddress address) throws IOException {
-    parse("hostName:2.1").withHostName(null).connect();
-    new Verifications() {{
-      InetAddress.getLocalHost();
-      new Display((Socket) any, 2, 1);
-    }};
+    assert name.getHostName() == null;
+    assert name.getDisplayNumber() == 2;
+    assert name.getScreenNumber() == 1;
+    assert name.toString().equals(":2.1");
+    assert name.getSocketFile().equals(new File("/tmp/.X11-unix/X2"));
   }
 }
